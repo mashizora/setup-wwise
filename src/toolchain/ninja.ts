61fa0e8235866d1226e92d2b122434bfc5d1855c
mashizora <30516315+mashizora@users.noreply.github.com>
2025-02-27 18:12:22 +0800

    feat(android): install ninja to support new android build script

---
+++ src/toolchain/ninja.ts
@@ -0,0 +1,28 @@
+import os from "node:os";
+import path from "node:path";
+import { execSync } from "node:child_process";
+import { rmSync } from "node:fs";
+
+const NINJA_URL =
+  "https://github.com/ninja-build/ninja/releases/latest/download/ninja-linux.zip";
+
+/** Install ninja build system. */
+export function installNinja() {
+  if (process.platform !== "linux") {
+    throw new Error("This function can only be called on linux");
+  }
+
+  try {
+    execSync("ninja --version", { stdio: "ignore" });
+  } catch {
+    console.info("Installing ninja...");
+
+    const output = path.join(os.tmpdir(), "ninja.zip");
+    execSync(`curl -sL --retry 5 "${NINJA_URL}" -o "${output}"`);
+    execSync(`unzip -q "${output}" -d /usr/bin/`);
+    rmSync(output, { force: true });
+
+    execSync("chmod +x /usr/bin/ninja");
+    console.info("Ninja installed.");
+  }
+}
