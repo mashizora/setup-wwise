1b830a76143cd1de2cfa9183cb18e44e7bbd7a1f
mashizora <30516315+mashizora@users.noreply.github.com>
2025-03-13 17:33:51 +0800

    test setup xcode

--- src/toolchain/xcode2.ts
+++ src/toolchain/xcode2.ts
@@ -1,6 +1,7 @@
 import { join } from "node:path";
 import { readdirSync, writeFileSync } from "node:fs";
 import semver, { SemVer } from "semver";
+import { exportVariable } from "@actions/core";
 
 /** Setup xcode for wwise plugin build system. */
 export function setupXcode(root: string) {
@@ -8,21 +9,44 @@ export function setupXcode(root: string) {
     throw new Error("This function can only be called on darwin");
   }
 
-  // These files specifing which xcode versions are available,
-  // and will be read by the wwise plugin build system.
+  // Declare "ToolchainVers.txt" files which specify available xcode versions
+  // for the wwise plugin build system. Each file contains version tags in
+  // `XcodeXXYZ` format, one tag per line. e.g. `Xcode1400\nXcode1500`.
   const toolchainFiles = [
     join(root, "Scripts/ToolchainSetup/iOS/ToolchainVers.txt"),
     join(root, "Scripts/ToolchainSetup/Mac/ToolchainVers.txt"),
   ];
 
-  // Scan installed Xcode versions and convert to `XcodeXXYZ` tags,
-  // which is the format used in "ToolchainVers.txt" files.
-  const tags = readdirSync("/Applications")
+  // Collect the version tags for filling "ToolchainVers.txt" files.
+  const versionTags = [] as string[];
+
+  // Scan all installed xcode versions.
+  const appList = readdirSync("/Applications")
     .filter((name) => /^Xcode_[\.\d]+\.app$/.test(name))
-    .map((name) => semver.coerce(name) as SemVer)
-    .map((v) => `Xcode${v.major}${v.minor}${v.patch}`);
+    .map((name) => ({ name, version: semver.coerce(name) as SemVer }));
+
+  // For each major version installed, select the latest minor or patch
+  // version and setup it for wwise plugin build system.
+  // For example, if 16.1 and 16.2 are installed, 16.2 will be selected.
+  for (const major of new Set(appList.map((app) => app.version.major))) {
+    const latestApp = appList
+      .filter((app) => app.version.major === major)
+      .sort((a, b) => semver.rcompare(a.version, b.version))[0];
+
+    const version = latestApp.version;
+    const versionCode = `${version.major}${version.minor}${version.patch}`;
+
+    // Export `AK_XCODE_DEVELOPER_DIR_XXYZ` variable.
+    // The wwise plugin build system uses these to set `DEVELOPER_DIR`,
+    // which will be passed to xcodebuild and xcrun commands.
+    const varName = `AK_XCODE_DEVELOPER_DIR_${versionCode}`;
+    const varValue = `/Applications/${latestApp.name}/Contents/Developer`;
+    exportVariable(varName, varValue);
+
+    versionTags.push(`Xcode${versionCode}`);
+  }
 
   for (const file of toolchainFiles) {
-    writeFileSync(file, tags.join("\n"), { encoding: "utf8" });
+    writeFileSync(file, versionTags.join("\n"), { encoding: "utf8" });
   }
 }
