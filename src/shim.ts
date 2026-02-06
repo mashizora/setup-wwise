e3d8537a9659ab02ed08bea55650768bc2330e93
mashizora <30516315+mashizora@users.noreply.github.com>
2026-02-06 18:36:28 +0800

    feat: rm shim.ts

--- src/shim.ts
+++
@@ -1,5 +0,0 @@
-import { createRequire } from "node:module";
-
-globalThis.__dirname = import.meta.dirname;
-globalThis.__filename = import.meta.filename;
-globalThis.require = createRequire(import.meta.url);
